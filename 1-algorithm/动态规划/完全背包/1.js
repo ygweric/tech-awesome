2e9cd5c96c34024bf7fc74b338d3f77f4b01af0f
杨国微(10031682) <yanggw@glodon.com>
2024-01-11 09:41:51 +0800

    背包

--- 1-algorithm/动态规划/完全背包/1.js
+++ 1-algorithm/动态规划/完全背包/1.js
@@ -7,45 +7,45 @@ https://zhuanlan.zhihu.com/p/377231783
 
 */
 
+/*
+  dp(i, j) : 
+      i: 前(i-1)个物品， 1 ~ i-1, i最小为1
+      j: 包容量 j
+      dp(i, j)：前i个物品的总价值
+
+  dp(i, j) =  max(
+                  dp(i-1, j), // 不包含第i个
+                  dp(i-1, j- weight[i] ) + value[i] // 包含第i个
+              )
+
+  j=0：dp(i, 0) = 0 // 背包容量为0， 无论多少个数，价值都是0 
+  i=1: dp(1, j) = 0 // 物品个数为1，无论背包容量多少，价值都是 min(weight[i], size)
+*/
+
+
+/*
+  1. 看了文章，算是一把搞定，主要算是有之前滚动数组的基础吧
+ */
 let getMaxBagValue = (weights, values, size) => {
-  /*
-    dp(i, j) : 
-        i: 前(i-1)个物品， 1 ~ i-1, i最小为1
-        j: 包容量 j
-        dp(i, j)：前i个物品的总价值
-
-    dp(i, j) =  max(
-                    dp(i-1, j), // 不包含第i个
-                    dp(i-1, j- weight[i] ) + value[i] // 包含第i个
-                )
-
-
-
-
-    j=0：dp(i, 0) = 0 // 背包容量为0， 无论多少个数，价值都是0 
-    i=1: dp(1, j) = 0 // 物品个数为1，无论背包容量多少，价值都是 min(weight[i], size)
-
-     */
 
   let dpArr = new Array(size + 1); // 0 ~ size
   for (let i = 0; i <= size; i++) {
-    dpArr[i] = weights[0];
+    dpArr[i] = Math.min( weights[0], i); // size和第一个物品选最小的，免得物品装不进size为1的背包，初始化要正确
   }
-  console.log("dpArr-1: ", dpArr);
+  console.log("dpArr-1: ", JSON.stringify(dpArr));
 
   for (let i = 2; i <= weights.length; i++) {
     for (let j = 1; j <= size; j++) {
+      if (j > weights[i]) {
+        dpArr[j] = Math.max(dpArr[j], dpArr[j - weights[i]] + values[i]);
+      } else {
+        // do nothing
+      }
 
-if (j>weights[i]) {
-    dpArr[j] = Math.max(dpArr[j], dpArr[j - weights[i]] + values[i]);
-} else {
-  // do nothing  
-}
-
-    //   dpArr[j] = Math.max(dpArr[j], dpArr[j - weights[i]] + values[i]);
+      //   dpArr[j] = Math.max(dpArr[j], dpArr[j - weights[i]] + values[i]);
       console.log("dpArr-2  weights[i]: ", weights[i]);
       console.log("dpArr-2  dpArr[j-weights[i]]: ", dpArr[j - weights[i]]);
-      console.log("dpArr-2: ", dpArr);
+      console.log("dpArr-2: ", JSON.stringify(dpArr));
     }
   }
 
@@ -54,4 +54,10 @@ if (j>weights[i]) {
   //
 };
 
-console.log(getMaxBagValue([5, 1, 2, 3, 4, 5], [6, 2, 4, 4, 5, 6], 6)); // 10
+console.log(
+  getMaxBagValue(
+    [5, 1, 2, 3, 4, 5], // weights
+    [6, 2, 4, 4, 5, 6], // values
+    6 // size
+  )
+); // 10
