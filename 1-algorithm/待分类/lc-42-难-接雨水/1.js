4539d39574b74cad4aaebc687dc642e1559669f1
杨国微(10031682) <yanggw@glodon.com>
2024-01-25 13:02:39 +0800

    1

---
+++ 1-algorithm/待分类/lc-42-难-接雨水/1.js
@@ -0,0 +1,17 @@
+/*
+https://leetcode.cn/problems/trapping-rain-water/description/
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+ 
+*/
+
+
+var trap = function(height) {
+//todo
+};
+
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])); // 6
+console.log(trap([4,2,0,3,2,5])); // 9
+// console.log(trap(xxxxxxx)); // xxxxxxxx
+// console.log(trap(xxxxxxx)); // xxxxxxxx
+// console.log(trap(xxxxxxx)); // xxxxxxxx
