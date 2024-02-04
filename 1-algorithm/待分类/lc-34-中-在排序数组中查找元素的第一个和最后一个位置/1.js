0d2288ba8866ec88c486b8c89b1580054729d8d6
杨国微(10031682) <yanggw@glodon.com>
2024-02-04 12:53:19 +0800

    1

---
+++ 1-algorithm/待分类/lc-34-中-在排序数组中查找元素的第一个和最后一个位置/1.js
@@ -0,0 +1,12 @@
+/*
+https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+*/
+
+var searchRange = function(nums, target) {
+// todo
+};
