9b3cea802f6c89b0f1bbfd6dae925fb9cb001d22
杨国微(10031682) <yanggw@glodon.com>
2024-02-04 15:29:58 +0800

    1

---
+++ 1-algorithm/递归+回溯/LCR-083-中-全排列-不重复/2.js
@@ -0,0 +1,14 @@
+/*
+https://leetcode.cn/problems/VvJkup/description/
+给定一个不含重复数字的整数数组 nums ，返回其 所有可能的全排列 。可以 按任意顺序 返回答案。
+*/
+
+/*
+ 1. 2024年1月8日 复习，没做出来，看答案了
+ */
+var permute = function (nums) {
+  
+    // todo 难！ 重做
+};
+
+console.log(permute([1, 2, 3])); // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
---
+++ 1-algorithm/递归+回溯/LCR-084-中-全排列-重复/2.js
@@ -0,0 +1,18 @@
+/*
+https://leetcode.cn/problems/7p8L0Z/description/
+给定一个可包含重复数字的整数集合 nums ，按任意顺序 返回它所有不重复的全排列。
+
+*/
+/*
+  1. 下面是官方题解，学习一下
+     太烧脑了，回头复习
+*/
+
+var permuteUnique = function (nums) {
+ 
+  // todo 难！ 重做
+};
+
+// console.log(permuteUnique([1, 2, 3])); // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// console.log(permuteUnique([1, 1, 2])); // [[1,1,2],[1,2,1],[2,1,1]]
+console.log(permuteUnique(["a", "a", "b"])); // [[1,1,2],[1,2,1],[2,1,1]]
